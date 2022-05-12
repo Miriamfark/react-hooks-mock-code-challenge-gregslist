2f1053001e1ee92266e03ec9dc91f04b04c4847d
Miriam <miriamjoshua00@gmail.com>
2022-05-12 20:41:28 +0300

    commit tree 1253d557b82f04b118328c049750ddda20d54f27

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState("")
+     
+  function handleSearchChange(newSearch) {
+    setSearchTerm(newSearch)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearchChange} />
+      <ListingsContainer
+      searchTerm={searchTerm} 
+      />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,53 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard"
 
-function ListingsContainer() {
+
+function ListingsContainer({ searchTerm }) {
+
+  const [listings, setListings] = useState()
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/listings")
+    .then((r)=>r.json())
+    .then((data)=>setListings(data))
+  }, [])
+
+  let filteredArray;
+  if(listings)
+  filteredArray = listings.filter((listing)=>{
+    return listing.description.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  console.log(filteredArray)
+
+  let listingsArray
+  if (listings) {
+    listingsArray=filteredArray.map((listing)=> {
+      return <ListingCard
+       key={listing.id} 
+       description={listing.description} 
+       image={listing.image} 
+       location={listing.location} 
+       id={listing.id}
+       onDeleteListing={handleDeleteListing}
+        />
+    })
+  }
+
+  function handleDeleteListing(id) {
+    let updatedArray;
+    if (listings) {
+      updatedArray = listings.filter((listing)=>{
+        return listing.id !== id
+      })
+      setListings(updatedArray)
+    }
+  }
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingsArray}
       </ul>
     </main>
   );
